7c58bbcb8716e3878e6d48bed6954907ffe8ded2
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-12-04 10:50:28 +0100

    Test query to get all wilders
    Help: to create wilders without having to use the GraphQL API, use `Wilder.create()` and then call the `save` method

--- src/server.integration.spec.ts
+++ src/server.integration.spec.ts
@@ -28,6 +28,47 @@ describe('Server', () => {
     return conn.close();
   });
 
+  describe('query wilders', () => {
+    it('returns all wilders', async () => {
+      const wilder1 = Wilder.create({
+        firstName: 'Laure',
+        lastName: 'Pinçon',
+      });
+      await wilder1.save();
+      const wilder2 = Wilder.create({
+        firstName: 'Pierre',
+        lastName: 'Roulle',
+      });
+      await wilder2.save();
+
+      const response = await query({
+        query: `{
+        wilders {
+          id
+          firstName
+          lastName
+        }
+      }
+      `,
+      });
+
+      expect(response.data).toEqual({
+        wilders: [
+          {
+            firstName: 'Laure',
+            id: '1',
+            lastName: 'Pinçon',
+          },
+          {
+            firstName: 'Pierre',
+            id: '2',
+            lastName: 'Roulle',
+          },
+        ],
+      });
+    });
+  });
+
   describe('mutation createWilder', () => {
     it('creates and returns a new wilder', async () => {
       const response = await mutate({
