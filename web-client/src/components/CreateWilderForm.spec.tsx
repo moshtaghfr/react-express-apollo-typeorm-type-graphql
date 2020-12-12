6a76851a85b1769074a7e45cd4d3e1db4377fc1e
Arnaud Renaud <arnaud.renaud@gmail.com>
2020-12-12 21:52:35 +0100

    Write remaining tests for CreateWilderForm
    Help:
    - https://testing-library.com/docs/dom-testing-library/api-events/
    - https://testing-library.com/docs/react-testing-library/example-intro/

--- web-client/src/components/CreateWilderForm.spec.tsx
+++ web-client/src/components/CreateWilderForm.spec.tsx
@@ -20,13 +20,30 @@ describe('CreateWilderForm', () => {
 
   describe('when clicking button to show form', () => {
     it('renders form', () => {
+      render(<CreateWilderForm />);
+      fireEvent.click(screen.getByRole('button'));
+
+      expect(screen.getByRole('form')).toBeInTheDocument();
     });
 
     it('renders button to hide form', () => {
+      render(<CreateWilderForm />);
+      fireEvent.click(screen.getByRole('button'));
+
+      const button = screen.getByRole('button');
+      expect(button).toBeInTheDocument();
+      expect(
+        within(button).getByText('Cacher le formulaire')
+      ).toBeInTheDocument();
     });
 
     describe('when clicking button to hide form', () => {
       it('hides form', () => {
+        render(<CreateWilderForm />);
+        fireEvent.click(screen.getByRole('button'));
+
+        fireEvent.click(screen.getByRole('button'));
+        expect(screen.queryByRole('form')).not.toBeInTheDocument();
       });
     });
   });
