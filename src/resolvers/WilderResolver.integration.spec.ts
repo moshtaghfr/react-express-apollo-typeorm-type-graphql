ecf23b22cdc209ae74d08456836bcb6fc2dc976d
Arnaud Renaud <arnaud.renaud@gmail.com>
2021-01-06 10:10:25 +0100

    Restructure tests, test query 'me'

--- src/resolvers/WilderResolver.integration.spec.ts
+++ src/resolvers/WilderResolver.integration.spec.ts
@@ -5,7 +5,7 @@ import { getExpressServer } from '../express-server';
 import UserSession from '../models/UserSession';
 import Wilder from '../models/Wilder';
 
-describe('Server', () => {
+describe('Wilder resolvers', () => {
   let testClient;
 
   beforeEach(async () => {
@@ -24,6 +24,136 @@ describe('Server', () => {
   afterEach(() => {
     const conn = getConnection();
     return conn.close();
+  });
+
+  describe('query wilders', () => {
+    it('returns all wilders', async () => {
+      const wilder1 = Wilder.create({
+        username: 'laurepincon',
+        password: 'laurepassword',
+        firstName: 'Laure',
+        lastName: 'Pinçon',
+      });
+      await wilder1.save();
+      const wilder2 = Wilder.create({
+        username: 'pierreroulle',
+        password: 'pierrepassword',
+        firstName: 'Pierre',
+        lastName: 'Roulle',
+      });
+      await wilder2.save();
+
+      const response = await testClient.post('/graphql').send({
+        query: `{
+        wilders {
+          id
+          firstName
+          lastName
+        }
+      }
+      `,
+      });
+
+      expect(JSON.parse(response.text).data).toEqual({
+        wilders: [
+          {
+            firstName: 'Laure',
+            id: '1',
+            lastName: 'Pinçon',
+          },
+          {
+            firstName: 'Pierre',
+            id: '2',
+            lastName: 'Roulle',
+          },
+        ],
+      });
+    });
+  });
+
+  describe('query me', () => {
+    let wilder;
+
+    beforeEach(async () => {
+      wilder = Wilder.create({
+        username: 'laurepincon',
+        password: 'laurepassword',
+        firstName: 'Laure',
+        lastName: 'Pinçon',
+      });
+      await wilder.save();
+    });
+
+    describe('when user is not authenticated', () => {
+      it('returns error', async () => {
+        const response = await testClient.post('/graphql').send({
+          query: `{
+          me {
+            firstName
+          }
+        }
+        `,
+        });
+
+        expect(response.text).toMatch('You are not authenticated.');
+      });
+    });
+
+    describe('when user is authenticated', () => {
+      it('returns user', async () => {
+        const wilderSession = UserSession.create({ user: wilder });
+        await wilderSession.save();
+
+        const response = await testClient
+          .post('/graphql')
+          .set('Cookie', [`sessionId=${wilderSession.uuid}`])
+          .send({
+            query: `{
+              me {
+                firstName
+              }
+            }
+          `,
+          });
+
+        expect(JSON.parse(response.text).data).toEqual({
+          me: {
+            firstName: wilder.firstName,
+          },
+        });
+      });
+    });
+  });
+
+  describe('mutation createWilder', () => {
+    it('creates and returns a new wilder', async () => {
+      const response = await testClient.post('/graphql').send({
+        query: `mutation {
+        createWilder(
+          input: {
+            username: "arnaudrenaud"
+            password: "bateaubateau"
+            firstName: "Arnaud"
+            lastName: "Renaud"
+          }
+        ) {
+          id
+          firstName
+          lastName
+        }
+      }
+      `,
+      });
+
+      expect(await Wilder.count({})).toEqual(1);
+      expect(JSON.parse(response.text).data).toEqual({
+        createWilder: {
+          id: '1',
+          firstName: 'Arnaud',
+          lastName: 'Renaud',
+        },
+      });
+    });
   });
 
   describe('mutation createSession', () => {
